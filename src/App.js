5a707a2cf125c704912e4bab5f5d1a3178977a9f
Wander-XIV <aroy1420@gmail.com>
2022-04-20 20:20:04 -0400

    Added mixpanel tracker
    Added trackers for which buttons are clicked and whenever the page loads.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("644d3e7c8c808b235ddb4e9057b95cad", {
+  debug: true,
+});
 
 function App() {
+
+mixpanel.track("Page Loaded");
+
+  let btnIncrease = (e) => {
+    mixpanel.track("Increase was clicked.");
+  };
+  let btnDecrease = (e) => {
+    mixpanel.track("Decrease was clicked.");
+  };
+
+  let btnReset = (e) => {
+    mixpanel.track("Reset was clicked.");
+  };
   const [count, setCount] = useState(0);
   return (
     <div className="container my-5">
@@ -11,20 +29,29 @@ function App() {
             <h2>{count}</h2>
             <button
               className="btn btn-success mx-3"
-              onClick={() => setCount(count + 1)}
+              onClick={() => {
+                setCount(count + 1);
+                btnIncrease();
+              }}
             >
               Increase
             </button>
             <button
               className="btn btn-danger mx-3"
-              onClick={() => setCount(count - 1)}
+              onClick={() => {
+                setCount(count - 1);
+                btnDecrease();
+              }}
               disabled={count === 0}
             >
               Decrease
             </button>
             <button
               className="btn btn-secondary mx-3"
-              onClick={() => setCount(0)}
+              onClick={() => {
+                setCount(0);
+                btnReset();
+              }}
               disabled={count === 0}
             >
               Reset
---
+++ src/Tracker.js
@@ -0,0 +1,23 @@
+// import './index.css'
+// import mixpanel from 'mixpanel-browser';
+
+// mixpanel.init("644d3e7c8c808b235ddb4e9057b95cad" , {
+//     debug : true
+// })
+
+// function App() {
+
+//   let btnClick = (e) => {
+//     mixpanel.track("The btn was clicked" , {
+
+//     })
+//   }
+
+//   return (
+//       <div className="Track">
+//           <button onClick={btnClick} >Send data.</button>
+//       </div>
+//   )
+// }
+
+// export default App
